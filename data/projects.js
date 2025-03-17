af866e393ff66717f6590c3d24f8f64cb1136cc7
Bhosya <bhosya27@gmail.com>
2025-03-17 20:13:03 +0700

    chore(data): add stitchloop data

--- data/projects.js
+++ data/projects.js
@@ -2,21 +2,32 @@ export const projects = [
   {
     id: 1,
     title: "Skafood",
-    description: "Skafood is simple eccomerce project that i made for my highschool.",
+    description:
+      "Skafood is simple eccomerce project that i made for my highschool.",
     image: "skafood.jpg",
     link: "https://www.instagram.com/skafood_project/",
   },
   {
     id: 2,
+    title: "Stitchloop",
+    description:
+      "Stitchloop is eccomerce that about reuaseable clothes. Built with nextjs",
+    image: "stitchloop.png",
+    link: "https://stitchloop.bowoksae.my.id",
+  },
+  {
+    id: 3,
     title: "Whatsapp Bot",
-    description: "Whatsapp bot is a whatsapp bot that i made for class to recognize the class, this bot can give reminder, task information, and other.",
+    description:
+      "Whatsapp bot is a whatsapp bot that i made for class to recognize the class, this bot can give reminder, task information, and other.",
     image: "class_whatsapp_bot.jpg",
     link: "https://github.com/Bhosya/class-whatsapp-bot",
   },
   {
-    id: 3,
+    id: 4,
     title: "Discord Bot",
-    description: "Discord bot is a discord bot that i made for Informatika. The only thing he can do is scraping information from elnino.",
+    description:
+      "Discord bot is a discord bot that i made for Informatika. The only thing he can do is scraping information from elnino.",
     image: "IK24_discord_bot.png",
     link: "https://github.com/Bhosya/IK24-Discord-Bot",
   },
